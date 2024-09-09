0d976696e39650181698bea9530b5a4516d26eae
Bruno Bastos Neves <brunoniconeves@gmail.com>
2024-09-09 07:45:18 -0300

    Setup playright in the artillery project

---
+++ frontendTests/learning/loginScreenTest/flows.js
@@ -0,0 +1,30 @@
+//
+// The code in this function was generated with
+// playwright codegen
+// https://playwright.dev/docs/codegen
+//
+async function checkLearningLoginScreen(
+  page,
+  userContext,
+  events,
+  test
+) {
+  await test.step('Go to Learnin Login Page', async () => {
+    const requestPromise = page.waitForRequest('http://ie-learning.magnilearn.com/Login.aspx?ReturnUrl=%2f');
+    await page.goto('http://ie-learning.magnilearn.com/Login.aspx?ReturnUrl=%2f');
+    const req = await requestPromise;
+  });
+
+  await test.step('Check if the page is well rendered', async () => {
+    const isWelcomeTextVisible = await page.getByText('xxx').isVisible();
+
+    if (!isWelcomeTextVisible) {
+      events.emit('counter', `user.element_check_failed.WelcomeToMagniLearn`, 1);
+    }
+  });
+}
+
+
+module.exports = {
+  checkLearningLoginScreen,
+};
