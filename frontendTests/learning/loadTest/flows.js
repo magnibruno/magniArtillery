64c66cbe585318234d4ac8c976a8277b7b4f2df6
Magnibruno <bruno@magnilearn.com>
2024-09-16 23:18:11 -0300

    Update unit test to bypass end session and avatar screens

--- frontendTests/learning/loadTest/flows.js
+++ frontendTests/learning/loadTest/flows.js
@@ -10,8 +10,8 @@ async function loadTest(
   test
 ) {
   await test.step('Get user to run', async () => {
-    const requestPromise = page.waitForRequest('https://localhost:44379/api/LoadTest/GetUser.aspx');
-    await page.goto('https://localhost:44379/api/LoadTest/GetUser.aspx');
+    const requestPromise = page.waitForRequest('https://perf.magnilearn.com/api/LoadTest/GetUser.aspx');
+    await page.goto('https://perf.magnilearn.com/api/LoadTest/GetUser.aspx');
     const req = await requestPromise;    
   });
 
@@ -26,7 +26,7 @@ async function loadTest(
       console.log('user loaded', loadTestUser.username);
     }
 
-    await page.goto('https://localhost:44379/');
+    await page.goto('https://perf.magnilearn.com/');
 
     const isWelcomeTextVisible = await page.getByText('Welcome to MagniLearn').isVisible();
     const text = await page.getByText('Welcome to MagniLearn').textContent();   
@@ -45,16 +45,43 @@ async function loadTest(
 
     await page.getByRole('button', { id: 'login-button' }).click();
 
+    // wait next page load
+    await page.waitForTimeout(3000);
+
+    
+    const terminateSessionBtn = await page.getByRole('button', { name: 'Terminate & stay here' }).isVisible();
+
+    if(terminateSessionBtn){
+      await page.screenshot({ path: 'screenshotTerminateSession-'+loadTestUser.username + '.png' });
+      await page.getByRole('button', { name: 'Terminate & stay here' }).click();
+    }
+
+
+    // wait next page load
+    await page.waitForTimeout(3000);
+
+    const avatarContainer = await page.locator('//div[contains(@id, "choose-an-avatar")]').isVisible();;
+
+    if(avatarContainer){
+      await page.screenshot({ path: 'screenshotChooseAvatar-'+loadTestUser.username + '.png' });
+      await page.locator('//div[contains(@id, "Male")]').click();
+    }
+
+    // wait student dashboard
+    await page.waitForTimeout(3000);
+
+    await page.screenshot({ path: 'screenshotStudentDashboard-'+loadTestUser.username + '.png' });
+
     //wait student dashboard loads
     await page.getByRole('navigation').getByRole('button', { name: 'Start Lesson' }).click();
 
-    await page.waitForTimeout(200000);
+    await page.waitForTimeout(10000);
 
-    await page.screenshot({ path: 'screenshot.png' });
+    await page.screenshot({ path: 'screenshotStartExercise-'+loadTestUser.username + '.png' });
 
     await page.waitForTimeout(1500000);
 
-
+    await page.screenshot({ path: 'screenshotEndingExercise-'+loadTestUser.username + '.png' });
   });
     
 }
